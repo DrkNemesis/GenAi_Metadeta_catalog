5f905cdea19ed26d59869763f050c62056626cfa
DrkNemesis <gowthamv901@gmail.com>
2026-03-12 23:11:21 +0530

    feat: add backend files

--- backend/config.py
+++ backend/config.py
@@ -2,5 +2,5 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-DB_URI = "postgresql+psycopg2://postgres:root123@localhost:5432/pagila"
+DATABASE_URI = os.environ.get("DB_URI")
 GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
